d97e3e07cbac19edfe7bbadef5be4f8671320271
Rangarajan <rrangarajan.85@gmail.com>
2017-02-23 23:58:43 +0530

    operators

---
+++ basics/operators.py
@@ -0,0 +1,81 @@
+a = 15
+b = 10
+
+print("Variables")
+print("a      = " + str(a))   
+print("b      = " + str(b))
+
+print("Arithmetic Operators")
+print("Addition       --> " + "a + b  = " + str(a+b))
+print("Subtraction    --> " + "a - b  = " + str(a-b))
+print("Multiplication --> " + "a * b  = " + str(a*b))
+print("Division       --> " + "a / b  = " + str(a/b))
+print("Modulus        --> " + "a % b  = " + str(a%b))
+print("Exponent       --> " + "a ** b = " + str(a**b))
+print("Floor Division --> " + "a // b = " + str(a//b)) 
+
+print("")
+print("Variables")
+print("a      = " + str(a))   
+print("b      = " + str(b))
+print("")
+print("Comparison Operators")
+print("Equal                    --> " + "a == b " + str(a==b))
+print("Not Equal                --> " + "a != b " + str(a!=b))
+print("Greater Than             --> " + "a >  b " + str(a>b))
+print("Less Than                --> " + "a <  b " + str(a<b))
+print("Greater Than or Equal To --> " + "a >= b " + str(a>=b))
+print("Less Than or Equal To    --> " + "a <= b " + str(a<=b))
+
+print("")
+print("Variables")
+print("a      = " + str(a))   
+print("b      = " + str(b))
+print("")
+print("Assignment Operators")
+a = a
+print("assign               --> "  + "a   = a " + str(a))
+a += b
+print("add and assign       --> "  + "a  += b " + str(a))
+a-=b
+print("subtract and assign  --> "  + "a  -= b " + str(a))
+a*=b
+print("multiply and assign  --> "  + "a  *= b " + str(a))
+a/=b
+print("divide and assign    --> "  + "a  /= b " + str(a))
+a%=b
+print("modulus and assign   --> "  + "a  %= b " + str(a))
+a **= b
+print("exponent and assign  --> "  + "a **= b " + str(a))
+a//=b
+print("floor div and assign --> "  + "a //= b " + str(a))
+
+print("")
+print("Variables")
+print("a      = " + str(a))   
+print("b      = " + str(b))
+print("")
+print("Bitwise Operators")
+
+
+print("")
+print("Variables")
+print("a      = " + str(a))   
+print("b      = " + str(b))
+print("")
+print("Logical Operators")
+
+
+print("")
+print("Variables")
+print("a      = " + str(a))   
+print("b      = " + str(b))
+print("")
+print("Membership Operators")
+
+print("")
+print("Variables")
+print("a      = " + str(a))   
+print("b      = " + str(b))
+print("")
+print("Identity Operators")
